2423ca16e7d92064492d7c41299ec3bfea9b320c
changhen2004 <782190611@qq.com>
2026-07-10 14:47:15 +0800

    添加控制器层：用户认证、文章管理、汇率查询、点赞功能

---
+++ ExChangeAPP_backend/controllers/like_controller.go
@@ -0,0 +1,39 @@
+package controllers
+
+import (
+	"net/http"
+
+	"exchangeapp/global"
+
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+)
+func LikeArticle(ctx *gin.Context) {
+	articleID := ctx.Param("id")
+
+	likeKey := "article:" + articleID + ":like"
+
+	if err := global.RedisDB.Incr(ctx, likeKey).Err(); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "Article liked successfully"})
+
+}
+
+func GetArticleLikes(ctx *gin.Context) {
+	articleID := ctx.Param("id")
+
+	likeKey := "article:" + articleID + ":like"
+
+	likeCount, err := global.RedisDB.Get(ctx, likeKey).Result()
+	
+	if err == redis.Nil {
+		likeCount = "0"
+	}else if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"like_count": likeCount})
+}
