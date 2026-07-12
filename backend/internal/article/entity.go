55a991880f199ae09eb72c50014abcd6d1a6be6b
changhen2004 <782190611@qq.com>
2026-07-12 21:45:03 +0800

    修复了一些bug

--- backend/internal/article/entity.go
+++ backend/internal/article/entity.go
@@ -9,7 +9,7 @@ type Article struct {
 	Content        string `binding:"required" gorm:"type:text;not null"`
 	Preview        string `binding:"required,max=500" gorm:"size:500;not null"`
 	CoverURL       string `gorm:"size:500;not null;default:''"`
-	ContentImages  string `gorm:"type:text;not null;default:''"`
+	ContentImages  string `gorm:"type:text;not null"`
 	Tags           string `gorm:"size:500;not null;default:'';index"`
 	Status         string `binding:"omitempty,oneof=draft published archived" gorm:"size:20;not null;default:draft;index"`
 	ViewCount      uint   `gorm:"not null;default:0"`
